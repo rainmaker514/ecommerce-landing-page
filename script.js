a6280e1e7a288f70ee9e857de3532863f9e19a19
rainmaker514 <tdoug016@fiu.edu>
2025-01-07 17:36:05 -0500

    mobile nav done

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo svg path:first-child");
 const navbarLink = document.querySelectorAll(".navbar-link");
-const rightLogos = document.querySelectorAll(".right-menu svg path:first-child");
+const rightMenu = document.querySelectorAll(".right-menu svg path:first-child");
+const menuIcon = document.querySelector(
+  ".mobile-nav-menu-icon svg path:first-child"
+);
 
 // NAV SCROLL EFFECT
 window.addEventListener("scroll", () => {
   if (window.scrollY > 60) {
     nav.classList.add("scrolled");
     logo.style.fill = "#fff";
-    rightLogos.forEach((rightLogo) => {
-      rightLogo.style.fill = "#fff";
+    menuIcon.style.fill = "#fff";
+    rightMenu.forEach((rightMenu) => {
+      rightMenu.style.fill = "#fff";
     });
     navbarLink.forEach((link) => {
       link.style.color = "#fff";
@@ -18,8 +22,9 @@ window.addEventListener("scroll", () => {
   } else {
     nav.classList.remove("scrolled");
     logo.style.fill = "#5fd788";
-    rightLogos.forEach((rightLogo) => {
-      rightLogo.style.fill = "#000";
+    menuIcon.style.fill = "#000";
+    rightMenu.forEach((rightMenu) => {
+      rightMenu.style.fill = "#000";
     });
     navbarLink.forEach((link) => {
       link.style.color = "#1f2937";
