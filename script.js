278299ebd409333f8f74838be256c07600e61e3d
Rainmaker514 <teriq.douglas01@gmail.com>
2025-05-20 21:17:33 -0400

    finished navbar scroll for tablet view

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("nav");
+const nav = document.querySelectorAll("nav");
 const logo = document.querySelector(".logo svg path:first-child");
 const navbarLink = document.querySelectorAll(".navbar-link");
 const rightMenu = document.querySelectorAll(".right-menu svg path:first-child");
@@ -12,12 +12,19 @@ const mobileMenuLinks = document.querySelectorAll(".mobile-nav-link");
 // const bagIcon = document.querySelector(".bag-icon");
 // const searchIcon = document.querySelector(".search-icon");
 const subMenuIcons = document.querySelectorAll(".sub-menu > svg");
+const desktopNav = document.querySelector(".desktop-nav");
+const desktopNavLink = document.querySelectorAll(".desktop-nav-link");
+const desktopLogo = document.querySelector(".desktop-nav .logo svg path:first-child");
 
 // NAV SCROLL EFFECT
 window.addEventListener("scroll", () => {
   if (window.scrollY > 60) {
-    nav.classList.add("scrolled");
+    nav.forEach((nav) => {
+      nav.classList.add("scrolled");
+    });
+    // nav.classList.add("scrolled");
     logo.style.fill = "#fff";
+    desktopLogo.style.fill = "#fff";
     menuIcon.style.fill = "#fff";
     rightMenu.forEach((rightMenu) => {
       rightMenu.style.fill = "#fff";
@@ -25,10 +32,18 @@ window.addEventListener("scroll", () => {
     navbarLink.forEach((link) => {
       link.style.color = "#fff";
     });
-    mobileNav.classList.add("scrolled");
+
+    desktopNavLink.forEach((link) => {
+      link.style.color = "#fff";
+    });
+    // mobileNav.classList.add("scrolled");
   } else {
-    nav.classList.remove("scrolled");
+    // nav.classList.remove("scrolled");
+    nav.forEach((nav) => {
+      nav.classList.remove("scrolled");
+    });
     logo.style.fill = "#5fd788";
+    desktopLogo.style.fill = "#5fd788";
     menuIcon.style.fill = "#000";
     rightMenu.forEach((rightMenu) => {
       rightMenu.style.fill = "#000";
@@ -36,7 +51,10 @@ window.addEventListener("scroll", () => {
     navbarLink.forEach((link) => {
       link.style.color = "#1f2937";
     });
-    mobileNav.classList.remove("scrolled");
+    desktopNavLink.forEach((link) => {
+      link.style.color = "#1f2937";
+    });
+    // mobileNav.classList.remove("scrolled");
   }
 });
 
